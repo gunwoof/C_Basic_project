ad40caf615072aac0c75779c61d17da76cbc431a
gunwoof <gunwoof1234@naver.com>
2021-08-22 20:40:21 +0900

    C_Basic_Project

---
+++ 나도코딩/반복문.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main_repeatation(void)
+{
+	//프로잭트
+	//피라미드 쌓기
+	//몇 층, 쌓기 반복
+
+	int floor;
+	printf("몇 층을 쌓으실 겁니까?");
+	scanf_s("%d", &floor);
+
+	for (int i = 1; i <= floor; i++) {
+		for (int j = 0; j < floor-i; j++) {
+			printf(" ");
+		}
+		for (int l = 0; l < 2*i - 1; l++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
