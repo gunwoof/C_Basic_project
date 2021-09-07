52de63fe73a972938ea039d04ca4718e1c5a0371
gunwoof <gunwoof1234@naver.com>
2021-09-08 01:09:07 +0900

    C언어 기초

--- 나도코딩/다차원배열.c
+++ 나도코딩/다차원배열.c
@@ -4,16 +4,19 @@
 
 int allAnimalcard[4][5]; // 카드 카드 20장 (4행 5열)
 char * strAnimal[10];
+int checkAnimal[4][5]; // 뒤집혔는지 여부 확인
 
 void initAnimalArray();
 void initAnimalName(); 
 void shuffleAnimal();
+void printAnimals(); 
+void printQuestion();
 int getEmptyPosition();
 int convert_position_x();
 int convert_position_y();
 
 int main(void) {
-	
+
 	// 프로잭트
 	// 동물 뒤집기
 	// 10마리의 서로 다른 동물 (각 카드 2장씩)
@@ -34,7 +37,7 @@ int main(void) {
 		int select1 = 0; // 사용자가 선택한 처음 수
 		int select2 = 0; // 사용자가 선택한 처음 수
 
-		printAnials(); // 동물 위치 출력
+		printAnimals(); // 동물 위치 출력
 		printQuestion(); // 문제 출력(카드 지도)
 		printf("뒤집을 카드를 2개 고르세요 : ");
 		scanf_s("%d %d", &select1, &select2);
@@ -43,8 +46,44 @@ int main(void) {
 		if (select1 == select2) {
 			continue;
 		}
-	}
 
+		// 좌표에 해당하는 카드를 뒤집어 보고 같은지 다른지 확인
+		// 정수 좌표를 (x,y)로 변환
+		int firstSelect_x = convert_position_x(select1);
+		int firstSelect_y = convert_position_y(select1);
+
+		int secondSelect_x = convert_position_x(select2);
+		int secondSelect_y = convert_position_y(select2);
+
+		// 같은 동물일 경우
+		if (checkAnimal[firstSelect_x][firstSelect_y] == 0
+			&& checkAnimal[secondSelect_x][secondSelect_y] == 0  // 뽑히지 않았고
+
+			&&
+
+			allAnimalcard[firstSelect_x][firstSelect_y] == allAnimalcard[secondSelect_x][secondSelect_y] // 첫번째 선택과 두번째 선택의 카드가 같으면
+			) {
+			printf("\n\n 빙고! : %s발견\n\n", strAnimal[allAnimalcard[firstSelect_x][firstSelect_y]]);
+			allAnimalcard[firstSelect_x][firstSelect_y] = 1;
+			allAnimalcard[secondSelect_x][secondSelect_y] = 1;
+		}
+
+		// 다른 동물일 경우
+		else {
+			printf("\n\n 땡!! (틀렸거나, 이미 뒤집힌 카드 입니다!)\n");
+			printf("%d : %s\n", select1, strAnimal[allAnimalcard[firstSelect_x][firstSelect_y]]);
+			printf("%d : %s\n", select2, strAnimal[allAnimalcard[secondSelect_x][secondSelect_y]]);
+			printf("\n\n");
+
+			failCount++; // 틀린횟수
+		}
+
+		// 모든 동물을 찾았는지 여부 (참 : 1, 거짓 : 0)
+		if (foundAllAnimals() == 1) {
+
+		}
+
+	}
 }
 
 void initAnimalArray() {
@@ -75,7 +114,7 @@ void shuffleAnimal() {
 			int x = convert_position_x(pos); // x좌표
 			int y = convert_position_y(pos); // y좌표
 
-			allAnimalcard[4][5] = i;
+			allAnimalcard[i][j] = i;
 		}
 	}
 }
@@ -94,7 +133,7 @@ int getEmptyPosition() {
 		int x = convert_position_x(Randomposition); // x좌표
 		int y = convert_position_y(Randomposition); // y좌표
 
-		if (allAnimalcard[4][5] == -1) {
+		if (allAnimalcard[x][y] == -1) {
 			return Randomposition;
 		}
 	}
@@ -113,3 +152,50 @@ int convert_position_y(int y) {
 	return y % 5; // y를 5로 나눈 나머지
 }
 
+void printAnimals() {
+
+	// ex
+	//ㅁㅁㅁㅁㅁ 0  0  1  1  2 
+	//ㅁㅁㅁㅁㅁ 2  3  3  4  4 
+	//ㅁㅁㅁㅁㅁ 5  5  6  6  7
+	//ㅁㅁㅁㅁㅁ 7  8  8  9  9
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 5; j++) {
+			printf("%8s", strAnimal[allAnimalcard[i][j]]);
+		}
+		printf("\n");
+	}
+	printf("\n========================\n\n");
+}
+
+void printQuestion() {
+	printf("\n\n문제\n\n");
+
+	int seq = 0;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 5; j++) {
+			// 카드를 뒤집어서 정답을 맞췄으면 '동물이름'
+			if (checkAnimal[i][j] != 0) {
+				printf("%8s", strAnimal[allAnimalcard[i][j]]);
+			}
+			// 뒤집히지 않았으면 (아직 정답을 못 맞힌) '위치를 나타내는 숫자'
+			else {
+				printf("%8d", seq);
+			}
+
+
+		}
+	}
+}
+
+int foundAllAnimals() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (checkAnimal[i][j] == 0) {
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
